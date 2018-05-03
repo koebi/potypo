c6c40e0f917b3355618c53ad4c338c84ea62d067
koebi <koebi@ezelo.de>
2018-05-03 17:21:00 +0200

    publish version 0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='potypo',
-    version='0.0.1',
+    version='0.0.2',
     description='spellchecking for .po-files',
     long_description=long_description,
     url='https://github.com/koebi/potypo',
