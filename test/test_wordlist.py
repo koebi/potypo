5ae58b28dbe3925d687906d71a145a3671e1f31d
koebi <koebi@ezelo.de>
2018-05-26 17:50:54 +0200

    refactor wordlist tests

--- test/test_wordlist.py
+++ test/test_wordlist.py
@@ -1,30 +1,24 @@
 import os
+import pytest
 from potypo.check import Check
 
-def test_wldir():
-    """
-    The wldir is given.
-    For "de", it should detect "test/data/wordlists/de.txt".
-    """
-    assert Check.get_wordlist("de", "test/data/wordlists", "test/data/de/LC_MESSAGES/django.po") == os.path.abspath("test/data/wordlists/de.txt")
+@pytest.mark.parametrize("lang, use_wl_dir, po_path, result", [
+    ("de", True, "test/data/de/LC_MESSAGES/django.po", "test/data/wordlists/de.txt"),
+    ("de", False, "test/data/de/LC_MESSAGES/django.po", None),
+    ("en", True, "test/data", "test/data/wordlist.txt"),
+    ("en", False, "test/data", "test/data/wordlist.txt"),
+    ("fr", True, "test/data/fr/LC_MESSAGES/django.po", "test/data/fr/LC_MESSAGES/wordlist.txt"),
+    ("fr", False, "test/data/fr/LC_MESSAGES/django.po", "test/data/fr/LC_MESSAGES/wordlist.txt"),
+    ("it", True, "test/data/it/LC_MESSAGES/django.po", "test/data/it/wordlist.txt"),
+    ("it", False, "test/data/it/LC_MESSAGES/django.po", "test/data/it/wordlist.txt"),
+])
 
-def test_default_wordlist_file():
-    """
-    No wldir is given.
-    For "en", it should detect "test/data/wordlist.txt".
-    """
-    assert Check.get_wordlist("en", "test/data/wordlists", "test/data") == os.path.abspath("test/data/wordlist.txt")
+def test_wordlist(lang, use_wl_dir, po_path, result):
+    wl_dir = "test/data/wordlists" if use_wl_dir else None
 
+    get = Check.get_wordlist(lang, wl_dir, po_path)
+    gotten = os.path.abspath(get) if get is not None else None
 
-def test_language_wordlist_file():
-    """
-    No wldir is given.
-    For "it", it should detect "test/data/it/wordlist.txt".
-    """
-    assert os.path.normpath(Check.get_wordlist("it", "test/data/wordlists", "test/data/it/LC_MESSAGES/django.po")) == os.path.abspath("test/data/it/wordlist.txt")
+    expected = os.path.abspath(result) if result is not None else None
 
-    """
-    No wldir is given.
-    For "fr", it should detect "test/data/fr/LC_MESSAGES/wordlist.txt"
-    """
-    assert Check.get_wordlist("fr", "test/data/wordlists", "test/data/fr/LC_MESSAGES/django.po") == os.path.abspath("test/data/fr/LC_MESSAGES/wordlist.txt")
+    assert gotten == expected
